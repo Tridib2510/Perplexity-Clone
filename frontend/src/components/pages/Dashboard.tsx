36c8101ae1690b3e369a1e03a5d479cd77ed3452
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2026-04-30 06:44:08 +0530

    Implemented the token for validating a user from the frontend

--- frontend/src/components/pages/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard.tsx
@@ -1,7 +1,9 @@
+import axios from "axios"
 import { createClient } from "@/lib/client"
 import type { User } from "@supabase/supabase-js";
 import { useEffect,useState } from "react";
 import { useNavigate } from "react-router";
+import { BACKEND_URL } from "@/lib/config";
 
 const supabase=createClient();
 export default  function Dashboard(){
@@ -21,6 +23,26 @@ export default  function Dashboard(){
         getInfo()
 
     },[])
+
+    useEffect(()=>{
+        async function getExistingConversations(){
+            if(user){
+                console.log(`${BACKEND_URL}`)
+            const {data:{session}}=await supabase.auth.getSession()
+            const jwt=session?.access_token // we get the jwt that we will send to the backend
+            const response=await axios.get(`${BACKEND_URL}/conversations`,{
+                headers:{
+                    Authorization:jwt
+                }
+            })
+
+            console.log(response.data)
+        }
+        }
+        getExistingConversations()
+        
+    },[user])
+
     return <div>
         {!user && <button onClick={()=>{
             navigate("/auth")//redirect
